d9e52494d9d180ff6c85c5c3304646d6c1dc0004
German-Trosko <skodagrodno2006@gmail.com>
2025-12-04 09:52:22 +0300

    feature 4

--- ewm-main-service/src/main/java/ru/practicum/ewm/service/CommentService.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/service/CommentService.java
@@ -38,8 +38,8 @@ public class CommentService {
         User author = userService.getUserById(userId);
         Event event = eventService.getEventById(eventId);
 
-        if (event.getState() == EventState.CANCELED) {
-            throw new ConflictException("Cannot comment on canceled event");
+        if (event.getState() != EventState.PUBLISHED) {
+            throw new ConflictException("Cannot comment on unpublished event");
         }
 
         Comment comment = CommentMapper.toEntity(dto, event, author);
